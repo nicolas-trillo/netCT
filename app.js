d6501689ae9b6c88b3cd6b48a17e384e6236a6c6
nicolas-trillo <nicolastrillo@icloud.com>
2021-12-09 22:22:27 -0500

    responsive nav

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ function startupSequence() {
     appendHeader();
     appendFooter();
 }
+
 function appendFooter() {
     $("#footer").load("footer.html");
 }
+
 function appendHeader() {
     $("#header").load("header.html");
     setTimeout(function() {
         setTab(currentTab);
     }, 1000);
 }
+
 function setTab(tab) {
     $(`#${tab}`).addClass('current-nav-pos');
+}
+
+var currentNavStatus = "invisible";
+
+function toggleMobileNav() {
+    var x = $('.nav-content-cont')
+    if (currentNavStatus === 'visible') {
+        x.removeClass('active-mobile-nav')
+        currentNavStatus = 'invisible'
+    } else {
+        x.addClass('active-mobile-nav')
+        currentNavStatus = 'visible'
+    }
 }
